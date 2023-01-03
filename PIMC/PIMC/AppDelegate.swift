25fe87e3ca2eed70ac5f6408c9d130f03a1facff
Klevra <sadviolent@gmail.com>
2023-01-03 16:26:30 +0900

    설치파일을 리소스 안으로 이동

--- PIMC/PIMC/AppDelegate.swift
+++ PIMC/PIMC/AppDelegate.swift
@@ -58,9 +58,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
         // auto run with start app
         viewController.run()
-
-        // starts on login
-        startsOnLogin()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -72,25 +69,5 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
 
-    func applicationWillFinishLaunching(_ notification: Notification) {
-        NSApp.hide(self)
-    }
-
-    func startsOnLogin() {
-        let bundleId = Bundle.main.bundleIdentifier!
-        // TODO: Make this more strict by only replacing at the end
-        let mainBundleId = bundleId.replacingOccurrences(of: "-LaunchAtLoginHelper", with: "")
-
-        // Ensure the app is not already running
-        guard NSRunningApplication.runningApplications(withBundleIdentifier: mainBundleId).isEmpty else {
-            NSApp.terminate(nil)
-            return
-        }
-
-        let pathComponents = (Bundle.main.bundlePath as NSString).pathComponents
-        let mainPath = NSString.path(withComponents: Array(pathComponents[0...(pathComponents.count - 5)]))
-        NSWorkspace.shared.launchApplication(mainPath)
-        NSApp.terminate(nil)
-    }
 }
 
